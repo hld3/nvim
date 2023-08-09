8f3550aafe1a13695dc06644b84ca180c54557be
Harry Dodson <harry.lee.dodson@gmail.com>
2023-08-09 17:59:09 -0400

    highlight yank

--- lua/dodson/init.lua
+++ lua/dodson/init.lua
@@ -1,3 +1,14 @@
 require("dodson.remap")
 require("dodson.onedark")
 require("dodson.set")
+
+-- [[ Hightlight on yank ]]
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function ()
+	vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = '*',
+})
+
