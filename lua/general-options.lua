f326b035d9123eb1fc6472998a6bb3978892a4ba
Harry Dodson <harry.lee.dodson@gmail.com>
2026-06-19 09:23:20 -0400

    minor updates

--- lua/general-options.lua
+++ lua/general-options.lua
@@ -65,7 +65,7 @@ vim.o.relativenumber = true
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
     callback = function()
-        vim.highlight.on_yank()
+        vim.hl.on_yank()
     end,
     group = highlight_group,
     pattern = "*",
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -3,8 +3,9 @@ return {
     config = function()
         local null_ls = require("null-ls")
         null_ls.setup({
+            -- debug = true,
             sources = {
-                null_ls.builtins.formatting.csharpier,
+                -- null_ls.builtins.formatting.csharpier,
                 null_ls.builtins.formatting.clang_format,
                 null_ls.builtins.formatting.shfmt,
             },
